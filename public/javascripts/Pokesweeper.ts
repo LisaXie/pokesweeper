5c6536c2e0edf16a2d1b09e5b9acf59304fb1e8c
Takumi Fujimoto <takumi@takumium.com>
2015-11-24 21:08:27 -0500

    Draw the cells field on the page

--- public/javascripts/Pokesweeper.ts
+++ public/javascripts/Pokesweeper.ts
@@ -1,13 +1,20 @@
 /// <reference path="MSObserver.ts" />
 /// <reference path="Minesweeper.ts" />
 /// <reference path="ImageMinesweeper.ts" />
+/// <reference path="../../typings/jquery/jquery.d.ts" />
 
 class Pokesweeper implements MSObserver {
     private ms: Minesweeper;
+    protected rows: number;
+    protected cols: number;
     private bombCount: number;
+    private fieldDomId = '#field';
     
-    constructor(cells: number[][]) {
+    constructor(cells: string[][]) {
+        this.rows = cells.length;
+        this.cols = cells[0].length;
         this.ms = new ImageMinesweeper(cells, 5);
+        this.drawField();
     }
     
     onGameStart() {
@@ -28,5 +35,36 @@ class Pokesweeper implements MSObserver {
     
     onVictory() {
         
+    }
+    
+    private drawField(): void {
+        for (var row = 0; row < this.rows; row++) {
+            $(this.fieldDomId).append('<div class="row" id="row' + row.toString() + '"></div>');
+            
+            for (var col = 0; col < this.cols; col++) {
+                try {
+                    var cell = <ColorCell>this.ms.getCellAt(row, col);
+                    if (cell) {
+                        var color = cell.color;
+                    }
+                } catch (e) {
+                    var color = 'white';
+                }
+                
+                var domCell = $('<div/>', {
+                    class: 'cell',
+                    id: this.getCellId(row, col),
+                    style: 'background-color: #' + color
+                });
+                $('#row' + row.toString()).append(domCell);
+            }
+        }
+    }
+    
+    /**
+     * Returns the ID for the DOM element representing the cell at (row, col) without the hashtag
+     */
+    private getCellId(row: number, col: number): string {
+        return 'cell' + row.toString() + '_' + col.toString();
     }
 }
