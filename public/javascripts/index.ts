7c9d24762124cf856d30de4e7a18e29805d5000a
Takumi <takumi@takumium.com>
2015-12-01 22:31:24 -0500

    Implement 'random' and 'random unsolved' buttons

--- public/javascripts/index.ts
+++ public/javascripts/index.ts
@@ -27,3 +27,33 @@ function selectPokemon(): void {
     var pokemonId = dropdown.options[dropdown.selectedIndex].value;
     window.location.replace(pokemonId);
 }
+
+function chooseRandom(): void {
+    window.location.replace(getRandomPokemonId());
+}
+
+function chooseRandomUnsolved(): void {
+    if (localStorage.getItem('pokedex') === getPokemonTotalCount().toString()) {
+        console.log('Congrats, you have no unsolved Pokémon left!');
+    } else {
+        do {
+            var pokemonId = getRandomPokemonId();
+        } while (localStorage.getItem(pokemonId) !== null);
+        
+        window.location.replace(pokemonId);
+    }
+}
+
+/**
+ * Returns a random string in 001 through 721.
+ */
+function getRandomPokemonId(): string {
+    var total = getPokemonTotalCount();
+    var index = Math.ceil(Math.random() * total);
+    
+    return ('00' + index.toString()).substr(-3);
+}
+
+function getPokemonTotalCount(): number {
+    return 721;
+}
